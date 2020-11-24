38b6f8cfa5a4fd8134e5df06f573ade57aa3a433
Pawel Szczerbiak <p.szczerbiak@gmail.com>
2020-11-24 12:31:58 +0100

    add basic setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cif2pdb",
+    packages=find_packages(),
+    install_requires=['pytest', 'pytest-mock'],
+    python_requires='>=3.6',
+    author="Paweł Szczerbiak",
+    author_email="pawel.szczerbiak@uj.edu.pl",
+    description="Covert CIF file into PDB format",
+    license='BSD-3-Clause',
+    url="https://github.com/PawelSzczerbiak/cif2pdb",
+)
